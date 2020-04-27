787fd8a739e8e8e787b74695d2652b05a0a2bee1
ShunyiZhao <shunyizhao@mail.nankai.edu.cn>
2020-04-28 06:07:51 +0800

    changes in works

--- project/src/components/Works/works.vue
+++ project/src/components/Works/works.vue
@@ -1,14 +1,36 @@
 <template>
-    <div>
-        <Basebanner></Basebanner>
-        <h1>This is works page!!</h1>
+    <div id="works">
+        <Baseheader></Baseheader>
+        <LeftFrame></LeftFrame>
     </div>
 </template>
 <script>
 import Basebanner from '../Common/Basebanner.vue'
+import Baseheader from '../Common/Baseheader.vue'
+import LeftFrame from '../TextFrame/leftFrame.vue'
 export default {
     components: {
-        Basebanner
+        Basebanner,
+        Baseheader,
+        LeftFrame,
     }
 }
 </script>
+<style scoped>
+#works {
+    height: 100%;
+    background-image: url(/assets/img/bg.png);
+}
+
+#header {
+    height: 7%;
+    background-color: black;
+}
+
+#LeftFrame {
+    font-size: 20pt;
+    color: white;
+}
+
+
+</style>
