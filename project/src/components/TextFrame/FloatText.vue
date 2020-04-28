6a38ef8f5950d28f181039287ca8b0a11c911150
ShunyiZhao <shunyizhao@mail.nankai.edu.cn>
2020-04-28 08:56:52 +0800

    float frame

---
+++ project/src/components/TextFrame/FloatText.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="FloatingText" class="FloatingDes">
+    234
+    {{message}}
+    555
+        <div class="titleDiv">
+        <p>{{ message }}</p>
+        <span>222{{ message }}334</span>
+        </div>
+    </div>
+</template>
+
+<script>
+var FloatingText = {
+    props: ['message'],
+    template:'#FloatingText',
+    data:function(){
+        return{
+            msg:'ok!'
+        }
+    },
+    methods:{
+    }
+}
+export default {
+    FloatingText
+}
+</script>
+
+<style>
+.FloatingDes {
+    width: 200px;
+    height: 300px;
+}
+
+.titleDiv{
+    font-size: 13px;
+    color: white;
+}
+
+.descriptionDiv{
+    font-size: 10px;
+    color: white;
+}
+
+</style>
--- project/src/components/Works/works.vue
+++ project/src/components/Works/works.vue
@@ -1,18 +1,29 @@
 <template>
     <div id="works">
         <Baseheader></Baseheader>
-        <LeftFrame></LeftFrame>
+        <LeftFrame id="navigation"></LeftFrame>
+
+        <FloatingText class="float_1" message="data1"></FloatingText>
+        <FloatingText class="float_2" message="897"></FloatingText>
     </div>
 </template>
 <script>
 import Basebanner from '../Common/Basebanner.vue'
 import Baseheader from '../Common/Baseheader.vue'
 import LeftFrame from '../TextFrame/leftFrame.vue'
+import FloatingText from '../TextFrame/FloatText.vue'
+
 export default {
+    data(){
+        return {
+            data1: "datafromother",
+        };
+    },
     components: {
         Basebanner,
         Baseheader,
         LeftFrame,
+        FloatingText,
     }
 }
 </script>
@@ -27,10 +38,26 @@ export default {
     background-color: black;
 }
 
-#LeftFrame {
+#navigation {
     font-size: 20pt;
+    color: white;
+    background-color: black;
+    width: 20%;
+    height: 93%;
+}
+
+.float_1 {
+    position: absolute;
+    left: 30%;
+    top: 30%;
     color: white;
 }
 
+.float_2 {
+    position: absolute;
+    left: 50%;
+    top: 30%;
+    color: white;
+}
 
 </style>
