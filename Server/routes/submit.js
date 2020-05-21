db523d6f802b1aa98d9dfb7a5e8a8e90d25444bc
ShunyiZhao <shunyizhao@mail.nankai.edu.cn>
2020-05-22 05:05:52 +0800

    use database

--- Server/routes/submit.js
+++ Server/routes/submit.js
@@ -1,9 +1,22 @@
-var express = require('express')
+var express = require('express');
 var router = express.Router();
+// use database
+var sql = require('sqlite3');
+var db = new sql.Database('serverData.db');
+var result;
+db.all("select * from candidate", show)
 
 router.get('/', function(req, res, next) {
-    var data = 100;
-    res.json(data);
+    db.all("select * from candidate", show);
+    res.send(result)
 })
+
+function show(error, rows) {
+    if (error) throw error;
+    result = rows;
+    //console.log(rows);
+    //console.log(rows[0]);
+    //console.log(rows[0].name);
+}
 
 module.exports = router;
